34d30526533716bf66fbd946cfe29c14ecf63c47
joycewanessa <35238699+joycewanessa@users.noreply.github.com>
2019-09-09 18:44:33 -0300

    Initial commit

---
+++ arduino/script.js
@@ -0,0 +1,36 @@
+﻿var firebase = require('firebase');
+
+//essa parte, é copiada do firebase
+//após criar o projeto, abra a opção projeto Web e copie o código pra cá
+//apenas o que está entre os traços abaixo
+//------------------------------------------------------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyBFN6qf7LEEZ6FsPey-C0oUNCDbtcLaQB8",
+    authDomain: "arduino-35845.firebaseapp.com",
+    databaseURL: "https://arduino-35845.firebaseio.com",
+    projectId: "arduino-35845",
+    storageBucket: "arduino-35845.appspot.com",
+    messagingSenderId: "209921599889",
+    appId: "1:209921599889:web:6749f7a0ee04712dc71230"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//------------------------------------------------------------------------------------
+
+//jhonny five
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function () {
+    var light = new five.Light("A0");
+    var valueLight = 0;
+    light.on("change", function () {
+        if (valueLight != this.level) {
+            valueLight = this.level;
+            console.log(valueLight);
+            firebase.database().ref('sensor').set({
+                light: this.level
+            })
+        }
+    });
+});
---
+++ arduino/script_node.js
@@ -0,0 +1,75 @@
+﻿var five = require("johnny-five");
+var firebase = require("firebase");
+
+var board = new five.Board();
+
+board.on("ready", function () {
+    var led = new five.Led(13)
+
+    this.repl.inject({
+        led: led,
+
+    });
+
+//essa parte, é copiada do firebase
+//após criar o projeto, abra a opção projeto Web e copie o código pra cá
+//apenas o que está entre os traços abaixo
+//------------------------------------------------------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyBFN6qf7LEEZ6FsPey-C0oUNCDbtcLaQB8",
+    authDomain: "arduino-35845.firebaseapp.com",
+    databaseURL: "https://arduino-35845.firebaseio.com",
+    projectId: "arduino-35845",
+    storageBucket: "arduino-35845.appspot.com",
+    messagingSenderId: "209921599889",
+    appId: "1:209921599889:web:6749f7a0ee04712dc71230"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//------------------------------------------------------------------------------------
+  
+  
+//path do banco
+var rootRef = firebase.database().ref();
+
+
+//jhonny five
+let lampada = document.querySelector("#lampada");
+
+    lampada.addEventListener("click", () => {
+
+        let estado = lampada.getAttribute('data-state');
+
+        console.log(lampada);
+
+        if (estado == 'off') {
+
+            firebase.database().ref("lampada").set("on");
+
+        } else {
+
+            firebase.database().ref("lampada").set("off");
+        }
+
+
+    });
+
+    firebase.database().ref('lampada').on('value', snapshot => {
+
+        let l = snapshot.val();
+
+        if (l == "on") {
+            lampada.src = 'lamp2.png';
+            lampada.setAttribute('data-state', 'on');
+
+        } else {
+            lampada.src = 'lamp1.jpg';
+            lampada.setAttribute('data-state', 'off');
+
+        }
+
+
+
+
+    })
+});
